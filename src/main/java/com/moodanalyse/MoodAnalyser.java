9eda40f4687532f8ef9b0725478a85748b943b5b
Amrut <amrutpanda7789@gmail.com>
2020-06-18 19:11:05 +0530

    ADDED:Analyse moos for sad

---
+++ src/main/java/com/moodanalyse/MoodAnalyser.java
@@ -0,0 +1,16 @@
+package com.moodanalyse;
+
+/**
+ * @Author : Amrut
+ * Purpose : This class is to analyse the mood
+ */
+public class MoodAnalyser {
+    /**
+     *
+     * @param message
+     * @return : String
+     */
+    public String analyseMood(String message) {
+        return "SAD";
+    }
+}
