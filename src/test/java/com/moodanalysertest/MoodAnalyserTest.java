95742820c78f34c391dffc7b9bd415bdb730ec8d
Amrut <amrutpanda7789@gmail.com>
2020-06-19 20:40:32 +0530

    REFACTORED:Pass mood message in constructor

--- src/test/java/com/moodanalysertest/MoodAnalyserTest.java
+++ src/test/java/com/moodanalysertest/MoodAnalyserTest.java
@@ -1,4 +1,4 @@
-package com.moodanalyser;
+package com.moodanalysertest;
 
 import com.moodanalyse.MoodAnalyser;
 import org.junit.Assert;
@@ -8,14 +8,14 @@ public class MoodAnalyserTest {
 
     @Test
     public void givenMessage_WhenSad_ShouldReturnSad() {
-        MoodAnalyser moodAnalyser = new MoodAnalyser();
-        String mood = moodAnalyser.analyseMood("This message is for sad mood");
+        MoodAnalyser moodAnalyser = new MoodAnalyser("I am in sad Mood");
+        String mood = moodAnalyser.analyseMood();
         Assert.assertEquals("SAD", mood);
     }
     @Test
-    public void givenMessage_WhenNotSad_ShouldReturnHappy() {
-        MoodAnalyser moodAnalyser = new MoodAnalyser();
-        String mood = moodAnalyser.analyseMood("This message is for happy mood");
+    public void givenMessage_WhenIn_AnyMood_ShouldReturnHappy() {
+        MoodAnalyser moodAnalyser = new MoodAnalyser("I am in happy Mood");
+        String mood = moodAnalyser.analyseMood();
         Assert.assertEquals("HAPPY", mood);
     }
 }
