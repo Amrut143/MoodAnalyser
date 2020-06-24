e2a12a63f20f06f8eab71918271177c6352c86bc
Amrut <amrutpanda7789@gmail.com>
2020-06-24 23:30:34 +0530

    ADDED:Custom exception type

--- src/main/java/com/moodanalyse/exception/MoodAnalysisException.java
+++ src/main/java/com/moodanalyse/exception/MoodAnalysisException.java
@@ -6,7 +6,7 @@ package com.moodanalyse.exception;
  */
 public class MoodAnalysisException extends Exception {
     /*Use enum for exception type*/
-    public enum exceptionType { ENTERED_NULL, ENTERED_EMPTY };
+    public enum exceptionType { ENTERED_NULL, ENTERED_EMPTY, NO_SUCH_CLASS, OBJECT_CREATION_ISSUE, METHOD_INVOCATION_ISSUE, NO_SUCH_METHOD, NO_ACCESS };
     exceptionType type;
     /**
      *
